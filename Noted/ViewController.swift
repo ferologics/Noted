4a52671e0acd611a5f3912ccb48c96ddb28b06ab
František Heteš <fhetes@gmail.com>
2015-06-22 13:48:56 -0700

    created the basic strucure lel

--- Noted/ViewController.swift
+++ Noted/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var whatnot: UIButton!
+    @IBOutlet weak var hahah: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,7 @@ class ViewController: UIViewController {
 
     @IBAction func ouch(sender: AnyObject) {
         println("ouch")
+        hahah.text = "meh!"
     }
 
 }
